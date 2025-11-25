679b0feb9008cca145681bf1faf54729d477a65c
sanketsyke <ksanket7109@gmail.com>
2025-11-26 01:34:34 +0530

    Update auth.c

--- project/auth.c
+++ project/auth.c
@@ -7,9 +7,9 @@ int staffLogin() {
     char password[32];
     printf("Enter staff password: ");
     scanf("%31s", password);  // Limit input to avoid overflow
-
     if (strcmp(password, "nas2025") == 0)
         return 1;  // Correct password, login successful
     else
         return 0;  // Incorrect password
 }
+
